9af8714e5b80b1f38e723bd0ba51dd935e190e8e
2dearsue <2dearsue@gmail.com>
2018-12-19 15:17:51 +0100

    testing

--- testing/test.js
+++ testing/test.js
@@ -1,20 +1,7 @@
-// Implementation code
+import { addition, fizzBuzz } from `./implementation.js`;
 
 const passedMessage = `your test has passed`;
 const failedMessage = 'your test has failed';
-
-let addition = (a, b) => {
-  if (typeof a !== `number` || typeof b !== `number`) {
-    return 'Sorry the operation is not possible!'
-  } else if (a < 0) {
-    return Math.abs(a) * b;
-  }
-
-  return a + b;
-}
-
-
-// Testing code
 
 let checkAddition = (function() {
   const expectedResult = 10;
@@ -56,26 +43,6 @@ const returnPositiveProduct = (() => {
 
 /// implementation:
 
-let fizzBuzz = num => {
-  if (typeof num !== `number`) {
-    return `The argument provided is not a number.`;
-
-  } else if(num <= 0) {
-    return `The number is either negative or 0. You shall not pass.`;
-
-  } else if (num % 5 == 0 && num % 3 == 0) {
-    return `FizzBuzz`;
-
-  } else if (num % 3 == 0) {
-    return `Fizz`;
-
-  } else if (num % 5 == 0) {
-    return `Buzz`;
-
-  } else {
-    return num;
-  }
-};
 
 // the testing:
 
